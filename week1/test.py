8ac289acc3d5678506b8ebd3ce616892069ae6e1
Aaron <ychen667@my.bcit.ca>
2024-09-22 12:47:00 +0800

    Update HTML structure with new sections and placeholder text

---
+++ week1/test.py
@@ -0,0 +1,8 @@
+# print('hello')
+crtl / 
+Input('please enter your name: ')
+print(Input('please enter your name: ')) # the inner fumction runs first, to completion, and the returend value is passed to the print function to be output
+# variabies - combination of stored value (somewhere in memory) # and a name(label)
+grade = input('please enter your grade:') # declaring a variable named "grade" (because this is the first time the word grade appears) and storing the value returned from the input function
+print(grade) #printing the value currently stored in the grade variable (note no quotes)
+# print(2**4)
